da67b347d14c7d614c5633087452046491a1dc03
Kaisei Yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2020-03-08 21:41:21 +0900

    test追加

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,9 +1,24 @@
 use epub_rs::*;
 
+const PATH: &'static str = "tests/data/childrens-literature.epub";
+
 #[test]
 fn new_epub_reader() {
-    let path = "tests/data/childrens-literature.epub";
-    let reader = EPUBReader::new(path);
+    let reader = EPUBReader::new(PATH);
 
     debug_assert!(reader.is_ok(), "{:?}", reader);
+}
+
+#[test]
+fn package_attributes() -> Result<(), failure::Error>{
+    let reader = EPUBReader::new(PATH)?;
+
+    if let Some(pd) = reader.package_document() {
+        assert_eq!("id", &pd.unique_identifier);
+        assert_eq!("3.0", &pd.version);
+    } else {
+        debug_assert!(false, "No Package Documents.")
+    }
+
+    Ok(())
 }
