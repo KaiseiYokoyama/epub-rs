eedfbacf51125df087ce7345f3285e43f31e0fba
Kaisei Yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2020-03-09 14:18:34 +0900

    EPUBのIDを読み取る

--- src/epub/package_document.rs
+++ src/epub/package_document.rs
@@ -1,45 +1,63 @@
 use std::collections::HashMap;
 use std::io::Read;
 
-use crate::EPUBError;
+use crate::{EPUBError, util::*};
 
 use xml::name::OwnedName;
 use xml::reader::XmlEvent;
 
 use failure::Error;
+use meta_data::Metadata;
 
 #[derive(Debug)]
 pub struct PackageDocument {
     attributes: HashMap<OwnedName, String>,
     pub unique_identifier: String,
     pub version: String,
+    pub meta_data: Metadata,
 }
 
 impl PackageDocument {
     /// sourceからXMLをパースしてPackage Documentの情報を得る
     pub fn new<R: Read>(source: R) -> Result<Self, Error> {
         let parser = xml::EventReader::new(source);
+        let xml = Xml::new(&mut parser.into_iter().peekable());
 
-        let attributes = parser.into_iter()
-            .find_map(|e| {
-                match e.ok()? {
-                    XmlEvent::StartElement {
-                        name,
-                        attributes,..
-                    } => if &name.local_name == "package" {
-                        Some(attributes)
-                    } else {
-                        None
-                    }
-                    _ => None
-                }
-            })
+        let (package_element, attributes) = xml
+            .get_by(&Box::new(|e: &XmlElement| match &e.event {
+                XmlEvent::StartElement {
+                    name,
+                    attributes, ..
+                } => &name.local_name == "package",
+                _ => false
+            }))
             .ok_or(EPUBError::PackageDocumentError {
                 err_msg: "Package element not found.".to_string()
+            })
+            .map(|e| match &e.event {
+                XmlEvent::StartElement {
+                    attributes, ..
+                } => (e, attributes),
+                _ => unreachable!()
             })?;
+        // let (package_element, attributes) = xml.iter()
+        //     .find_map(|e| match &e.event {
+        //         XmlEvent::StartElement {
+        //             name,
+        //             attributes, ..
+        //         } => if &name.local_name == "package" {
+        //             Some((e, attributes))
+        //         } else {
+        //             None
+        //         }
+        //         _ => None
+        //     })
+        //     .ok_or(EPUBError::PackageDocumentError {
+        //         err_msg: "Package element not found.".to_string()
+        //     })?;
 
         let attributes = attributes.into_iter()
-            .map(|atr| (atr.name, atr.value))
+            .map(|atr| (atr.name.clone(), atr.value.clone()))
             .collect::<HashMap<OwnedName, String>>();
 
         let unique_identifier = attributes.get(&OwnedName {
@@ -62,11 +80,14 @@ impl PackageDocument {
             })?
             .to_string();
 
+        let meta_data = Metadata::new(package_element, &unique_identifier)?;
+
         Ok(
             Self {
                 attributes,
                 unique_identifier,
                 version,
+                meta_data,
             }
         )
     }
@@ -103,4 +124,97 @@ impl PackageDocument {
 pub enum Dir {
     ltr,
     rtl,
+}
+
+pub mod meta_data {
+    use super::*;
+
+    /// EPUBのパッケージドキュメントに記載された<metadata>要素, およびそのコンテンツ
+    #[derive(Debug)]
+    pub struct Metadata {
+        /// UUID や DOI、ISBN など、特定のレンディションに関連付けられている識別子
+        identifier: Vec<Identifier>,
+        unique_identifier: Identifier,
+        attributes: HashMap<OwnedName, String>,
+    }
+
+    impl Metadata {
+        pub fn new(package_element: &XmlElement, unique_identifier: &str) -> Result<Self, Error> {
+            let (meta_data_elem, attributes) = package_element.children.iter()
+                .find_map(|e| match &e.event {
+                    XmlEvent::StartElement {
+                        name,
+                        attributes, ..
+                    } => if &name.local_name == "metadata" {
+                        Some((e, attributes))
+                    } else {
+                        None
+                    }
+                    _ => None
+                })
+                .ok_or(EPUBError::PackageDocumentError {
+                    err_msg: "Metadata element not found.".to_string()
+                })?;
+
+            let attributes = attributes.into_iter()
+                .map(|atr| (atr.name.clone(), atr.value.clone()))
+                .collect::<HashMap<OwnedName, String>>();
+
+            let identifier = meta_data_elem.children.iter()
+                // dc::identifier属性のみをfilter
+                .filter(|&e| {
+                    match &e.event {
+                        XmlEvent::StartElement {
+                            name, ..
+                        } => {
+                            &name.prefix == &Some("dc".to_string()) && &name.local_name == "identifier"
+                        }
+                        _ => false
+                    }
+                })
+                .map(|e| {
+                    let identifier = e.inner_text();
+                    let id = if let XmlEvent::StartElement { attributes, .. } = &e.event {
+                        attributes.iter()
+                            .find_map(|a| {
+                                if &a.name.local_name == "id" {
+                                    Some(a.value.to_string())
+                                } else { None }
+                            })
+                    } else { None };
+
+                    Identifier { id, identifier }
+                })
+                .collect::<Vec<Identifier>>();
+
+            let unique_identifier = identifier.iter()
+                .find_map(|id| if &id.id == &Some(unique_identifier.to_string()) {
+                    Some(id.clone())
+                } else { None })
+                .ok_or(EPUBError::PackageDocumentError {
+                    err_msg: "Unique identifier element not found.".to_string()
+                })?;
+
+
+            Ok(Self {
+                unique_identifier,
+                identifier,
+                attributes,
+            })
+        }
+
+        pub fn unique_identifier(&self) -> &Identifier {
+            &self.unique_identifier
+        }
+
+        pub fn identifiers(&self) -> &Vec<Identifier> {
+            &self.identifier
+        }
+    }
+
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    pub struct Identifier {
+        pub id: Option<String>,
+        pub identifier: String,
+    }
 }
